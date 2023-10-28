2482f0bd3a0e76d5c738e42fcce55f1e61810c2c
tadeo <tadeomendoza905@gmail.com>
2023-10-28 12:18:31 -0300

    test5

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,26 +1,8 @@
 <template>
   <h1>NAV</h1>
-  <br>
+
   <ul>
-    <li>Test</li>
-    <li>Test</li>
-    <li>Test</li>
-    <li>Test</li>
-    <li>Test</li>
-    <li>Test</li>
-    <li>Test</li>
-    <li>Test</li>
-    <li>Test</li>
-    <li>Test</li>
+    <li>aaa</li>
   </ul>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>

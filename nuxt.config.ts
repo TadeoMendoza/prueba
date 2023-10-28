11fc261e4f93c2618938a0c2b1e980b335c56183
tadeo <tadeomendoza905@gmail.com>
2023-10-28 12:05:26 -0300

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,4 @@
 
 export default defineNuxtConfig({
-	// router: {
-	// 	base: '/v2/',
-	// },
-	app: {
-		baseURL: '/v2/',
-	},
-	ssr: true,
-	build: {
-		publicPath: '/v2/',
-		// buildDir: 'dist',
-		buildAssetsDir:'/v2/',
-	},
+
 });
